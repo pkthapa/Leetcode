51bef44d9ea67aa724cd04c7f2e26f62685d46e6
Pankaj Kumar Thapa <pk.thapa66@gmail.com>
2022-07-08 08:07:05 +0530

    Leetcode #70

---
+++ Easy/Climbing-Stairs/Climbing-Stairs.cpp
@@ -0,0 +1,29 @@
+#include "main.hpp"
+
+// MUST WATCH
+// Best explanation: https://www.youtube.com/watch?v=Y0lT9Fck7qI
+class Solution {
+public:
+  int climbStairs(int n) {
+    // Can be solved using DP. Similar to fibonacci series.
+    int first = 1;
+    int second = 1;
+    for (int i = 0; i < n - 1; ++i){
+      first += second;
+      second = first - second;
+    }
+    return first;
+  }
+};
+
+int main() {
+  Solution sln;
+
+  assert(2 == sln.climbStairs(2));
+
+  assert(3 == sln.climbStairs(3));
+
+  assert(8 == sln.climbStairs(5));
+
+  return 0;
+}

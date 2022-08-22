7200b379dcd8c251c7f6bac9ebf22441a076e084
Pankaj Thapa <pk.thapa66@gmail.com>
2022-08-22 09:20:01 +0530

    Leetcode #121
    Implemented easily understandable way.

--- Easy/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
+++ Easy/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    /*int maxProfit(vector<int>& prices) {
         int profit = 0;
         int minPrice = INT32_MAX;
         
@@ -14,8 +14,28 @@ public:
                 profit = prices[i] - minPrice;
             }
         }
-
         return profit;
+    }*/
+    int maxProfit(vector<int>& prices) {
+        int l = 0; // Buying.
+        int r = 1; // Selling.
+        int maxProfit = 0;
+
+        while (r < prices.size()) {
+            // If buying price is less than selling price.
+            if (prices[l] < prices[r]) {
+                int currProfit = prices[r] - prices[l];
+                maxProfit = max(maxProfit, currProfit);
+            }
+            // If selling price is less than previously bought price, then
+            // update the buying price to current selling price because we
+            // want to maximize our profit.
+            else {
+                l = r;
+            }
+            ++r;
+        }
+        return maxProfit;
     }
 };
 

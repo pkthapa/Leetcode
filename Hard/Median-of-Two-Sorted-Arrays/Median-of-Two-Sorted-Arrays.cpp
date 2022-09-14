4b30a82da9599bbe53138ebf80c71d447f3fac5f
Pankaj Thapa <pk.thapa66@gmail.com>
2022-09-14 08:37:34 +0530

    Leetcode #4 Binary search approach.

--- Hard/Median-of-Two-Sorted-Arrays/Median-of-Two-Sorted-Arrays.cpp
+++ Hard/Median-of-Two-Sorted-Arrays/Median-of-Two-Sorted-Arrays.cpp
@@ -2,41 +2,46 @@
 
 class Solution {
 public:
+    // https://www.youtube.com/watch?v=yD7wV8SyPrc&ab_channel=KeertiPurswani
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        // Let's do the binary search on the smaller array to get the best time complexity.
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
         
-        vector<int> mergedArr(m + n);
+        int low = 0;
+        int high = n1;
         
-        int l = 0;
-        int r = 0;
-        int i = 0;
-        while (l < m && r < n) {
-            if (nums1[l] < nums2[r]) {
-                mergedArr[i++] = nums1[l++];
+        while (low <= high) {
+            int cut1 = (low + high) >> 1; //(low + (high - low)) / 2;
+            int cut2 = ((n1 + n2) >> 1) - cut1; // 'n >> 1' means  'n / 2'.
+            
+            int l1 = (cut1 == 0) ? INT32_MIN : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? INT32_MIN : nums2[cut2 - 1];
+            int r1 = (cut1 == n1) ? INT32_MAX : nums1[cut1];
+            int r2 = (cut2 == n2) ? INT32_MAX : nums2[cut2];
+            
+            /*
+            Condition to satisfy:
+            l1 < r2
+            l2 < r1
+            */
+            if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            else if (l2 > r1) {
+                low = cut1 + 1;
             }
             else {
-                mergedArr[i++] = nums2[r++];
+                return ((n1 + n2) % 2 == 0) ?
+                    static_cast<double>(max(l1, l2) + min(r1, r2)) / 2 :
+                    static_cast<double>(min(r1, r2));
             }
         }
-        
-        while (l < m) {
-            mergedArr[i++] = nums1[l++];
-        }
-        
-        while (r < n) {
-            mergedArr[i++] = nums2[r++];
-        }
-        
-        int sz = mergedArr.size();
-        double median = 0.0F;
-        if (sz % 2 == 0) {
-            median = (double)(mergedArr[sz / 2] + mergedArr[(sz / 2) - 1]) / 2;
-        }
-        else {
-            median = mergedArr[sz / 2];
-        }
-        return median;
+        return 0.0F;
     }
 };
 

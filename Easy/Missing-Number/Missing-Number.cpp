2dfcbedda427bce7d5f00c53f9df59ed4b214855
Pankaj Thapa <pk.thapa66@gmail.com>
2022-09-15 00:06:50 +0530

    Leetcode #268 Cyclic sort algorithm

--- Easy/Missing-Number/Missing-Number.cpp
+++ Easy/Missing-Number/Missing-Number.cpp
@@ -3,9 +3,37 @@
 class Solution {
 public:
     /**
-     * @brief Approach 1: Using formula: (n (n + 1))/2
+     * @brief Approach 1: Cyclic sort algorithm
+     */
+    int missingNumber(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+        
+        // Cyclic sort algorithm.
+        // Keep swapping the value against the index until the value and the index match.
+        while (i < n) {
+            if (nums[i] < n && nums[i] != nums[nums[i]]) {
+                std::swap(nums[i], nums[nums[i]]);
+            }
+            else {
+                ++i;
+            }
+        }
+        
+        // Once the array is sorted, then check for the missing index.
+        for (i = 0; i < n; ++i) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    /**
+     * @brief Approach 2: Using formula: (n (n + 1))/2
      * 'n' is the size of the input vector.
      */
+    /*
     int missingNumber(vector<int>&& nums) {
         int size = nums.size();
         int expectedSum = (size * (size + 1)) / 2;
@@ -17,6 +45,7 @@ public:
         
         return expectedSum - total;
     }
+    */
 };
 
 int main(int argc, char* argv[]) {

c37dc9a8047d4efd1a15558f2338e12d2543a187
Pankaj Kumar Thapa <pk.thapa66@gmail.com>
2022-10-21 07:05:06 +0530

    Leetcode #207 update

--- Medium/Course-Schedule/Course-Schedule.cpp
+++ Medium/Course-Schedule/Course-Schedule.cpp
@@ -1,6 +1,102 @@
 #include "main.hpp"
 
+/*
+Approach: Topological sorting.
+
+This is how we can implement this algorithm:
+
+a. Initialization
+
+1. We will store the graph in Adjacency Lists, which means each parent vertex will have a list containing all of its children.
+We will do this using a HashMap where the ‘key’ will be the parent vertex number and the value will be a List containing children vertices.
+
+2. To find the sources, we will keep a HashMap to count the in-degrees i.e., count of incoming edges of each vertex. Any vertex with ‘0’ in-degree will be a source.
+
+---------------------------------------------------------------------
+
+b. Build the graph and find in-degrees of all vertices
+
+1. We will build the graph from the input and populate the in-degrees HashMap.
+
+---------------------------------------------------------------------
+
+c. Find all sources
+
+1. All vertices with ‘0’ in-degrees will be our sources and we will store them in a Queue.
+
+---------------------------------------------------------------------
+
+d. Sort
+
+1. For each source, do the following things:
+- Add it to the sorted list.
+- Get all of its children from the graph.
+- Decrement the in-degree of each child by 1.
+- If a child’s in-degree becomes ‘0’, add it to the sources Queue.
+
+2. Repeat step 1, until the source Queue is empty.
+*/
+
 class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        if (numCourses <= 0) {
+            return false;
+        }
+        
+        // A. Initialize the graph.
+        unordered_map<int, int> inDegree; // Count incoming edges of every vertex.
+        unordered_map<int, vector<int>> graph; // Adjacency list graph.
+        
+        for (int i = 0; i < numCourses; ++i) {
+            inDegree[i] = 0;
+            graph[i] = vector<int>();
+        }
+        
+        // B. Build the graph.
+        for (int i = 0; i < prerequisites.size(); ++i) {
+            int parent = prerequisites[i][0], child = prerequisites[i][1];
+            
+            graph[parent].push_back(child);
+            ++inDegree[child];
+        }
+        
+        // C. Find all sources, i.e., vertices with 0 in-degree.
+        queue<int> sources;
+        for (auto entry : inDegree) {
+            if (0 == entry.second) {
+                sources.push(entry.first);
+            }
+        }
+        
+        vector<int> sortedOrder; // Result.
+        
+        // D. Add all the 'sources' (source has 0 in-degree) to the 'sortedOrder' (result) and
+        // subtract one from all the children's in-degree. If a child's in-degree becomes zero,
+        // then add the child to the 'sources' queue.
+        while (!sources.empty()) {
+            auto vertex = sources.front();
+            sources.pop();
+            
+            sortedOrder.push_back(vertex);
+            
+            vector<int> children = graph[vertex];
+            for (auto child : children) {
+                --inDegree[child];
+                
+                if (0 == inDegree[child]) {
+                    sources.push(child);
+                }
+            }
+        }
+        
+        // If the sortedOrder does not contain all tasks (courses), then there is a cyclic dependency between tasks.
+        // Hence, we will not be able to schedule all tasks.
+        return (sortedOrder.size() == numCourses);
+    }
+};
+
+/*class Solution {
     // https://www.youtube.com/watch?v=kXy0ABd1vwo&ab_channel=TECHDOSE
 
     bool isCyclic(vector<vector<int>>& adjList, vector<int>& visited, int curr) {
@@ -51,7 +147,7 @@ public:
         }
         return true;
     }
-};
+};*/
 
 int main() {
     Solution sln;

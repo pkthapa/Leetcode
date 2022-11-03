17289fbc78377a15de4616b8625022a78bd78cdd
Pankaj Thapa <pk.thapa66@gmail.com>
2022-11-03 09:23:37 +0530

    Leetcode #383 update on logic

--- Easy/Ransom-Note/Ransom-Note.cpp
+++ Easy/Ransom-Note/Ransom-Note.cpp
@@ -5,20 +5,25 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> freqs;
 
-        // Store the character count frequencies.
-        for (auto c : magazine) {
+        // Store the character frequecies of 'ransomNote' first
+        // to avoid computational costs.
+        for (auto c : ransomNote) {
             ++freqs[c];
         }
 
-        // Check if the hash-table has all the required characters to
-        // construct 'ransomNote'.
-        for (auto c : ransomNote) {
-            if (freqs[c] <= 0) {
-                return false;
+        // For every character (c) in 'magazine', decrease 'c's frequency count
+        // and check if it has reached '0'. If reached '0' then erase 'c' from
+        // 'freqs' and then check the size of the 'freqs'. If 'freqs' is empty
+        // then that means we are able to construct (return true).
+        for (auto c : magazine) {
+            if (freqs.count(c) > 0 && --freqs[c] == 0) {
+                freqs.erase(c);
+                if (freqs.empty()) {
+                    return true;
+                }
             }
-            --freqs[c];
         }
-        return true;
+        return false;
     }
 };
 

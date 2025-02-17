08d5343e441045de0a6ba1d87d006e8f596bb194
pkthapa <pk.thapa66@gmail.com>
2025-02-17 07:30:43 +0530

    Add binary solution updated.

--- Easy/Add-Binary/Add-Binary.cpp
+++ Easy/Add-Binary/Add-Binary.cpp
@@ -1,32 +1,40 @@
 #include "main.hpp"
+// Function prototype
+void myStrReverse(string& res);
 
 class Solution {
 public:
     string addBinary(string a, string b) {
         string res;
         int carry = 0;
-        int lenA = a.size() - 1;
-        int lenB = b.size() - 1;
+        int aLen = a.length() - 1, bLen = b.length() - 1;
 
-        while (lenA >= 0 or lenB >= 0 or carry != 0) {
-
-            if (lenA >= 0) {
-                carry += a[lenA] - '0';
-                --lenA;
+        while (aLen >= 0 or bLen >= 0 or carry != 0) {
+            if (aLen >= 0) {
+                carry += a[aLen] - '0';
+                --aLen;
+            }
+            if (bLen >= 0) {
+                carry += b[bLen] - '0';
+                --bLen;
             }
 
-            if (lenB >= 0) {
-                carry += b[lenB] - '0';
-                --lenB;
-            }
-
-            res += std::to_string(carry % 2);
+            res += to_string(carry % 2);
             carry /= 2;
         }
-        reverse(res.begin(), res.end());
+
+        // std::reverse(res.begin(), res.end());
+        myStrReverse(res);
         return res;
     }
 };
+
+void myStrReverse(string& res) {
+    int i = 0, j = res.length() - 1;
+    while (i < j) {
+        std::swap(res[i++], res[j--]);
+    }
+}
 
 int main() {
     Solution sln;
